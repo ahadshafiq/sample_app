17abdf5c97a0991a910e20993213193f89cb4f9c
ahadshafiq <ahad.shafiq@gmail.com>
2012-12-18 15:26:37 +0300

    added layout links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
+  root :to => "pages#home"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+#  match '/home', :to => 'pages#home'
 
 end
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+	it "shoud have a Home page at '/'" #do
+	# 	get '/'
+	# 	response.should hav_selector('title', :content => "Home")
+	# end
+
+	# it "should have a Contact pages at '/contact" do
+	# 	get '/contact'
+	# 	response.should have_selector('title', :content => "Contact")
+	# end
+
+	# it "should have an ABout page at '/about" do
+	# 	get '/about'
+	# 	response.should have_selector('about', :content => "About")
+	# end
+
+	# it "should have a Help page at '/help'" do
+	# 	get '/help'
+	# 	response.should have_selector('title', :content => "Help")
+	# end
+
+end
